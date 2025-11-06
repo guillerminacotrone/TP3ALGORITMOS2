79e05425c0aefb26e21ad4e3f62475a24dd67f64
maguipesqueira <mpesqueira@uade.edu.ar>
2025-11-05 22:10:54 -0300

    agrego Nodo a la carpeta modelo

--- src/Modelo/Nodo.java
+++ src/Modelo/Nodo.java
@@ -1,4 +1,58 @@
-package Modelo;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Nodo {
+public class Nodo<T> {
+    private T dato;
+    private List<Nodo<T>> adyacentes;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.adyacentes = new ArrayList();
+    }
+
+    public T getDato() {
+        return this.dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public List<Nodo<T>> getAdyacentes() {
+        return this.adyacentes;
+    }
+
+    public void agregarAdyacente(Nodo<T> nodo) {
+        if (nodo == null) {
+            throw new IllegalArgumentException("No se puede agregar un nodo adyacente nulo");
+        } else {
+            if (!this.adyacentes.contains(nodo)) {
+                this.adyacentes.add(nodo);
+            }
+
+        }
+    }
+
+    public void removerAdyacente(Nodo<T> nodo) {
+        this.adyacentes.remove(nodo);
+    }
+
+    public String toString() {
+        return this.dato.toString();
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj != null && this.getClass() == obj.getClass()) {
+            Nodo<?> nodo = (Nodo)obj;
+            return this.dato.equals(nodo.dato);
+        } else {
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        return this.dato.hashCode();
+    }
 }
