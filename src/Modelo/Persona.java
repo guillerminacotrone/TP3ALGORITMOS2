b9e0519e9d990642ea4c725b52b09284339b0a4e
guillermina cotrone <gcotrone@uade.edu.ar>
2025-11-05 21:15:30 -0300

    agrego a modelo Persona

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -1,4 +1,85 @@
 package Modelo;
 
 public class Persona {
+
+
+
+    private String nombre;
+
+    private int edad;
+
+
+
+    public Persona(String nombre, int edad) {
+
+        this.nombre = nombre;
+
+        this.edad = edad;
+
+    }
+
+
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+
+    }
+
+
+
+    public int getEdad() {
+
+        return edad;
+
+    }
+
+
+
+    public void setEdad(int edad) {
+
+        this.edad = edad;
+
+    }
+
+
+
+    @Override
+
+    public String toString() {
+
+        return nombre + " (" + edad + " años)";
+
+    }
+
+    @Override
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Persona persona = (Persona) obj;
+
+        return edad == persona.edad && nombre.equals(persona.nombre);
+
+    }
+
+    @Override
+
+    public int hashCode() {
+
+        return nombre.hashCode() * 31 + edad;
+
+    }
+
 }
